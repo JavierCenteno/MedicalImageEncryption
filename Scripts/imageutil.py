7e77fe204b869acc96a7cdec8b8f1c202ba0610d
JavierCenteno <jacenve@telefonica.net>
2018-12-14 15:10:41 +0100

    Added documentation.

--- Scripts/imageutil.py
+++ Scripts/imageutil.py
@@ -11,43 +11,94 @@ import numpy
 from PIL import Image
 from scipy import misc
 
-def load_image(image_path):
+def load_image(path):
 	"""
-        Parameters
-        ----------
-        image_path : str
-            Path to an image file.
+	Parameters
+	----------
+	path : str
+		A file path to read an image from.
+	
+	Returns
+	-------
+	numpy.ndarray
+		A tridimensional array of the values of the channels of the pixels of
+		an image that can be accessed by the x index, y index and channel index
+		of the value in that order.
+	"""
+	return misc.imread(path)
 
-        Returns
-        -------
-        numpy.ndarray
-            A tridimensional array of the values of the channels of the pixels 
-            of an image that can be accessed by the x index, y index and
-            channel index of the value in that order.
-    """
-
-	return misc.imread(image_path)
-
-def classification_metric(submatrix, tau):
-	return numpy.mean(numpy.std(submatrix)) > tau
-
-def divide_regions(img, s, tau):
-	width = len(img)
-	height = len(img[0])
-
-	mask = numpy.empty([height // s, width // s])
-
-	width = len(mask)
-	height = len(mask[0])
-
-	for i in range(width):
-		for j in range(height):
-			mask[i][j] = classification_metric(numpy.ix_([i*s, (i+1)*s], [j*s, (j+1)*s]), tau)
-
-	return mask
-
-def save_image(mask, path):
-	Image.fromarray(mask, mode="RGB").save(path)
+def save_image(image, path):
+	"""
+	Parameters
+	----------
+	path : str
+		A file path to save the image to.
+	image : array_like
+		An image represented as a tridimensional array of the values of the
+		channels of its pixels that can be accessed by the x index, y index and
+		channel index of the value in that order.
+	"""
+	Image.fromarray(image, mode="RGB").save(path)
 
 def save_mask(mask, path):
+	"""
+	Parameters
+	----------
+	path : str
+		A file path to save the image to.
+	mask : array_like
+		An image represented as a tridimensional array of the values of the
+		channels of its pixels that can be accessed by the x index, y index and
+		channel index of the value in that order.
+	"""
 	Image.fromarray(mask, mode="L").save(path)
+
+def classification_metric(submatrix, threshold):
+	"""
+	Parameters
+	----------
+	submatrix : array_like
+		A section of a matrix.
+	threshold : float
+		The threshold of the minimal standard deviation required to consider
+		submatrix as part of the region of interest.
+	
+	Returns
+	-------
+	boolean
+		True if this submatrix can be considered part of the region of interest
+		using threshold as a threshold, False otherwise.
+	"""
+	return numpy.mean(numpy.std(submatrix)) > threshold
+
+def divide_regions(image, block_size, threshold):
+	"""
+	Parameters
+	----------
+	image : array_like
+		An image represented as a tridimensional array of the values of the
+		channels of its pixels that can be accessed by the x index, y index and
+		channel index of the value in that order.
+	block_size : int
+		The value used for the width and height of the sections this image will
+		be divided in.
+	threshold : float
+		The threshold of the minimal standard deviation required to consider
+		a section of the image as part of the region of interest.
+	
+	Returns
+	-------
+	boolean
+		True if this submatrix can be considered part of the region of interest
+		using threshold as a threshold, False otherwise.
+	"""
+	width = len(image)
+	height = len(image[0])
+	mask = numpy.empty([height // block_size, width // block_size])
+	width = len(mask)
+	height = len(mask[0])
+	for i in range(width):
+		for j in range(height):
+			block = numpy.ix_([i*block_size, (i+1)*block_size], [j*block_size, (j+1)*block_size])
+			mask[i][j] = classification_metric(block, threshold)
+	return mask
