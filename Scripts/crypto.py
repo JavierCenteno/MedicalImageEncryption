d474b6b2f52d1ccf21b18b204c85bee1304d8047
JavierCenteno <jacenve@telefonica.net>
2018-12-21 02:03:55 +0100

    Code organization and documentation.

--- Scripts/crypto.py
+++ Scripts/crypto.py
@@ -17,18 +17,75 @@ from matrixutil import vector
 from byteutil import to_bytes
 
 def a_matrix(a, b, c, d):
+	"""
+	Parameters
+	----------
+	a : int
+		A parameter used to fill in the matrix.
+	b : int
+		A parameter used to fill in the matrix.
+	c : int
+		A parameter used to fill in the matrix.
+	d : int
+		A parameter used to fill in the matrix.
+	
+	Returns
+	-------
+	a : numpy.ndarray
+		A two dimensional matrix with dimensions (4, 4) used in pseudorandom
+		number generation for the encryption algorithm.
+	"""
 	return numpy.array(
 			[
-				[(2*b + c)*(d + 1) + 3*d + 1, 2*(b + 1), 2*b*c + c + 3, 4*b + 3],
-				[2*(a + 1)*(d + b*c*(d + 1)) + a*(c + 1)*(d + 1), 2*(a + b + a*b) + 1, a*(c + 3) + 2*b*c*(a + 1) + 2, 3*a + 4*b*(a + 1) + 1],
-				[3*b*c*(d + 1) + 3*d, 3*b + 1, 3*b*c + 3, 6*b + 1],
-				[c*(b + 1)*(d + 1) + d, b + 1, b*c + c + 1, 2*b + 2]
+				[
+					(2*b + c)*(d + 1) + 3*d + 1,
+					2*(b + 1),
+					2*b*c + c + 3,
+					4*b + 3
+				],
+				[
+					2*(a + 1)*(d + b*c*(d + 1)) + a*(c + 1)*(d + 1),# E
+					2*(a + b + a*b) + 1,
+					a*(c + 3) + 2*b*c*(a + 1) + 2,# F
+					3*a + 4*b*(a + 1) + 1
+				],
+				[
+					3*b*c*(d + 1) + 3*d,
+					3*b + 1,
+					3*b*c + 3,
+					6*b + 1
+				],
+				[
+					c*(b + 1)*(d + 1) + d,
+					b + 1,
+					b*c + c + 1,
+					2*b + 2
+				]
 			]
 		)
 
-def omega_matrix(a, x, h, w):
-	l = h*w
-	u = vector(2, 3, 5, 1)
+u = vector(2, 3, 5, 1)
+
+def omega_matrix(a, x, height, width):
+	"""
+	Parameters
+	----------
+	a : numpy.ndarray
+		A two dimensional matrix with dimensions (4, 4).
+	x : numpy.ndarray
+		A column vector, or two dimensional matrix with dimensions (1, 4).
+	height : int
+		Height of the resulting matrix.
+	width : int
+		Width of the resulting matrix.
+	
+	Returns
+	-------
+	numpy.ndarray
+		A two dimensional matrix representing the omega matrix used in the
+		encryption algorithm.
+	"""
+	l = height*width
 	y = []
 
 	for i in range(math.ceil(l/16.0)):
@@ -41,8 +98,4 @@ def omega_matrix(a, x, h, w):
 
 		y.append(vector(to_bytes(numpy.uint32(n), 4)))
 
-	return numpy.reshape(y, [h, w])
-
-omega = omega_matrix(a_matrix(2, 1, 2, 2), vector(0.6, 0.2, 0.8, 0.6).T, 8, 8)
-
-print(omega)
+	return numpy.reshape(y, [height, width])
--- Scripts/main.py
+++ Scripts/main.py
@@ -8,8 +8,12 @@ Driver for the algorithm defined in crypto.
 """
 
 import imageutil
+import matrixutil
+import crypto
 
-image = imageutil.load_image("../Test/mri2.jpg")
-mask = imageutil.divide_regions(image, 4, 3)
+#image = imageutil.load_image("../Test/mri2.jpg")
+#mask = imageutil.divide_regions(image, 4, 3)
+#imageutil.save_mask(mask, "../Test/mask.png")
 
-imageutil.save_mask(mask, "../Test/mask.png")
+omega = crypto.omega_matrix(crypto.a_matrix(2, 1, 2, 2), matrixutil.vector(0.6, 0.2, 0.8, 0.6).T, 8, 8)
+print(omega)
